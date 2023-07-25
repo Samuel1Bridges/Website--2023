fa5416abdf989a387162e4cbb659f9d7ea8e1061
Samuel Bridges (1000099539) <BRIDSR2@student.op.ac.nz>
2023-07-26 09:46:57 +1200

    Add Week 2 homework

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
 
-<h1>Hello, Welcome to svelte</h1>
+<h1> HTML practice</h1>
+
+<ul>
+    <li>cat</li>
+    <li>dog</li>
+    <li>fish</li>
+    <li>bird</li>
+</ul>
+
+<img src = "html practice.JPG" alt = "Raptor"/>
+
+<a href = "https://online.op.ac.nz/"> <br/>Otago Polytechnic </a>
