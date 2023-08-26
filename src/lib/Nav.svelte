82134c442c31e1ef2170d3d0a87004d6122691a0
Samuel1Bridges <bridsr2@student.op.ac.nz>
2023-08-26 12:30:10 +1200

    added nav bar *

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,18 +1,51 @@
+
 <style>
-    li {
-        float:left;
-        padding: 12px 50px;
-        margin: 0px;
-        
-    }
+
+  nav
+  {
+    height: 80px;
+    background-color: black;
+    color: white;
+    display: flex;
+    align-items: center;
+
+  }
 
 
+  .right-nav-menu
+  {
+    margin-left: auto;
+  }
+  button
+  {
+    border: 2px solid white;
+    background-color: black;
+    padding: 5px;
+    margin: 5px;
+  }
+  a
+  {
+    text-decoration: none;
+    font-size: 20px;
+    color: azure;
+    
+  }
+  button:hover
+  {
+    background-color: gray;
+  }
 </style>
-<div>
-    <ul>
-        <li><a href="/"> Home </a></li>
-        <li><a href="/race"> Race </a></li>
-        <li><a href="/class"> Class </a></li>
-        <li><a href="/map"> Map </a></li>
-    </ul>
+
+<nav>
+
+<div class = "left-logo">
+  Final Fantasy 14
 </div>
+
+<div class = "right-nav-menu">
+<button><a href="/">Home</a></button>
+<button><a href="race/">Race</a></button>
+<button><a href="class/">Class</a></button>
+<button><a href="map/">Map</a></button>
+</div>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
-
-
-
-
 <h1>Home page</h1>
+
+<style>
+
+    video
+    {
+        display: flex;
+        width: 100%;
+        height: 100px;
+    }
+</style>
+
+
+
+<video>
+    <source src="movie.mp4" type="video/mp4">
+    Your browser does not support the video tag.
+  </video>
+  
